477cceebbb0d0e57a38b89f3527ab598c9496930
vitorpre <vitor.presutti@gmail.com>
2015-04-09 17:58:46 -0300

    Adicionado listas com os nomes dos champions

--- assets/js/selecao.js
+++ assets/js/selecao.js
@@ -48,6 +48,107 @@ var letraJ = "<li class='menu-campeao' value='aatrox'>Janna</li>" +
 "<li class='menu-campeao' value='alistar'>Jayce</li>" + 
 "<li class='menu-campeao' value='amumu'>Jinx</li>";
 
+var letraK = "<li class='menu-campeao' value='aatrox'>Kalista</li>" + 
+"<li class='menu-campeao' value='ahri'>Karma</li>" + 
+"<li class='menu-campeao' value='akali'>Karthus</li>" + 
+"<li class='menu-campeao' value='alistar'>Kassadin</li>" + 
+"<li class='menu-campeao' value='alistar'>Katarina</li>" + 
+"<li class='menu-campeao' value='alistar'>Kayle</li>" + 
+"<li class='menu-campeao' value='alistar'>Kennen</li>" + 
+"<li class='menu-campeao' value='alistar'>Kha'Zix</li>" + 
+"<li class='menu-campeao' value='amumu'>Kog'Maw</li>";
+
+var letraL = "<li class='menu-campeao' value='aatrox'>Lee Sin</li>" + 
+"<li class='menu-campeao' value='ahri'>Leona</li>" + 
+"<li class='menu-campeao' value='akali'>Lissandra</li>" + 
+"<li class='menu-campeao' value='alistar'>Lucian</li>" + 
+"<li class='menu-campeao' value='alistar'>Lulu</li>" + 
+"<li class='menu-campeao' value='amumu'>Lux</li>";
+
+
+var letraM = "<li class='menu-campeao' value='aatrox'>Malphite</li>" + 
+"<li class='menu-campeao' value='ahri'>Malzahar</li>" + 
+"<li class='menu-campeao' value='akali'>Maokai</li>" + 
+"<li class='menu-campeao' value='alistar'>Master Yi</li>" + 
+"<li class='menu-campeao' value='alistar'>Miss Fortune</li>" + 
+"<li class='menu-campeao' value='alistar'>Mordekaiser</li>" + 
+"<li class='menu-campeao' value='amumu'>Morgana</li>";
+
+
+var letraN = "<li class='menu-campeao' value='aatrox'>Nami</li>" + 
+"<li class='menu-campeao' value='ahri'>Nasus</li>" + 
+"<li class='menu-campeao' value='akali'>Nautilus</li>" + 
+"<li class='menu-campeao' value='alistar'>Nidalee</li>" + 
+"<li class='menu-campeao' value='alistar'>Nocturne</li>" + 
+"<li class='menu-campeao' value='amumu'>Nunu</li>";
+
+var letraO = "<li class='menu-campeao' value='aatrox'>Olaf</li>" + 
+"<li class='menu-campeao' value='ahri'>Orianna</li>";
+
+var letraP = "<li class='menu-campeao' value='aatrox'>Pantheon</li>" + 
+"<li class='menu-campeao' value='ahri'>Poppy</li>";
+
+var letraQ = "<li class='menu-campeao' value='aatrox'>Quinn</li>";
+
+var letraR = "<li class='menu-campeao' value='aatrox'>Rammus</li>" + 
+"<li class='menu-campeao' value='ahri'>Rek'Sai</li>" + 
+"<li class='menu-campeao' value='akali'>Renekton</li>" + 
+"<li class='menu-campeao' value='alistar'>Rengar</li>" + 
+"<li class='menu-campeao' value='alistar'>Riven</li>" + 
+"<li class='menu-campeao' value='alistar'>Rumble</li>" + 
+"<li class='menu-campeao' value='amumu'>Ryze</li>";
+
+var letraS = "<li class='menu-campeao' value='aatrox'>Sejuani</li>" + 
+"<li class='menu-campeao' value='ahri'>Shaco</li>" + 
+"<li class='menu-campeao' value='akali'>Shen</li>" + 
+"<li class='menu-campeao' value='alistar'>Shyvana</li>" + 
+"<li class='menu-campeao' value='alistar'>Singed</li>" + 
+"<li class='menu-campeao' value='alistar'>Sion</li>" + 
+"<li class='menu-campeao' value='alistar'>Sivir</li>" + 
+"<li class='menu-campeao' value='alistar'>Skarner</li>" + 
+"<li class='menu-campeao' value='alistar'>Sona</li>" + 
+"<li class='menu-campeao' value='alistar'>Soraka</li>" + 
+"<li class='menu-campeao' value='alistar'>Swain</li>" + 
+"<li class='menu-campeao' value='amumu'>Syndra</li>";
+
+var letraT = "<li class='menu-campeao' value='aatrox'>Talon</li>" + 
+"<li class='menu-campeao' value='ahri'>Taric</li>" + 
+"<li class='menu-campeao' value='akali'>Teemo</li>" + 
+"<li class='menu-campeao' value='alistar'>Thresh</li>" + 
+"<li class='menu-campeao' value='alistar'>Tristana</li>" + 
+"<li class='menu-campeao' value='alistar'>Trundle</li>" + 
+"<li class='menu-campeao' value='alistar'>Tryndamere</li>" + 
+"<li class='menu-campeao' value='alistar'>Twisted Fate</li>" + 
+"<li class='menu-campeao' value='alistar'>Twitch</li>";
+
+var letraU = "<li class='menu-campeao' value='aatrox'>Udyr</li>" + 
+"<li class='menu-campeao' value='ahri'>Urgot</li>";
+
+var letraV = "<li class='menu-campeao' value='aatrox'>Varus</li>" + 
+"<li class='menu-campeao' value='ahri'>Vayne</li>" + 
+"<li class='menu-campeao' value='akali'>Veigar</li>" + 
+"<li class='menu-campeao' value='alistar'>Vel'Koz</li>" + 
+"<li class='menu-campeao' value='alistar'>Vi</li>" + 
+"<li class='menu-campeao' value='alistar'>Viktor</li>" + 
+"<li class='menu-campeao' value='alistar'>Vladimir</li>" + 
+"<li class='menu-campeao' value='amumu'>Volibear</li>";
+
+var letraW = "<li class='menu-campeao' value='aatrox'>Warwick</li>" + 
+"<li class='menu-campeao' value='ahri'>Wukong</li>";
+
+var letraX = "<li class='menu-campeao' value='aatrox'>Xerath</li>" + 
+"<li class='menu-campeao' value='ahri'>Xin Zhao</li>";
+
+var letraY = "<li class='menu-campeao' value='aatrox'>Yasuo</li>" + 
+"<li class='menu-campeao' value='ahri'>Yorick</li>";
+
+var letraZ = "<li class='menu-campeao' value='aatrox'>Zac</li>" + 
+"<li class='menu-campeao' value='ahri'>Zed</li>" + 
+"<li class='menu-campeao' value='akali'>Ziggs</li>" + 
+"<li class='menu-campeao' value='alistar'>Zilean</li>" + 
+"<li class='menu-campeao' value='amumu'>Zyra</li>";
+
+
 $(document).ready(function(){
 
 	$(".letra").on("click", function(){
@@ -91,10 +192,52 @@ $(document).ready(function(){
 				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraJ + "</ul></div>");
 				break;
 				case "K":
-				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraA + "</ul></div>");
+				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraK + "</ul></div>");
 				break;
 				case "L":
-				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraD + "</ul></div>");
+				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraL + "</ul></div>");
+				break;
+				case "M":
+				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraM + "</ul></div>");
+				break;
+				case "N":
+				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraN + "</ul></div>");
+				break;
+				case "O":
+				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraO + "</ul></div>");
+				break;
+				case "P":
+				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraP + "</ul></div>");
+				break;
+				case "Q":
+				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraQ + "</ul></div>");
+				break;
+				case "R":
+				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraR + "</ul></div>");
+				break;
+				case "S":
+				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraS + "</ul></div>");
+				break;
+				case "T":
+				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraT + "</ul></div>");
+				break;
+				case "U":
+				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraU + "</ul></div>");
+				break;
+				case "V":
+				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraV + "</ul></div>");
+				break;
+				case "W":
+				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraW + "</ul></div>");
+				break;
+				case "X":
+				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraX + "</ul></div>");
+				break;
+				case "Y":
+				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraY + "</ul></div>");
+				break;
+				case "Z":
+				$(campo).parent().parent().after("<div class='div-champions col-lg-12 col-sm-12 col-xs-12' style='opacity:0;'><hr><ul>" + letraZ + "</ul></div>");
 				break;
 				default:
 				break;
